39692aebbbfa42efbddce4a20302a19e989c65a1
Zurrruz <zuruz.yarr@gmail.com>
2024-10-07 20:43:00 +0300

    DZ

--- GeneratingEnemies/Assets/Scripts/Spawner.cs
+++ GeneratingEnemies/Assets/Scripts/Spawner.cs
@@ -8,18 +8,25 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Enemy _enemyPrefab;
 
     [SerializeField] float _timeSpawn;
+    private bool _canGenerate;
 
     private List<Vector3> _directions;
 
     private int _minNumberSpawnPoints = 0;
 
+    WaitForSeconds _timeout;
+
     private void Awake()
     {
-        _directions = new() { 
-            Vector3.forward, 
-            Vector3.back, 
-            Vector3.right, 
+        _directions = new() {
+            Vector3.forward,
+            Vector3.back,
+            Vector3.right,
             Vector3.left};
+
+        _canGenerate = true;
+
+        _timeout = new WaitForSeconds(_timeSpawn);
     }
 
     private void Start()
@@ -29,14 +36,15 @@ public class Spawner : MonoBehaviour
 
     private IEnumerator CreateEnemy()
     {
-        int spawnPoint = Random.Range(_minNumberSpawnPoints, _spawnPositions.Count);
+        while (_canGenerate)
+        {
+            int spawnPoint = Random.Range(_minNumberSpawnPoints, _spawnPositions.Count);
 
-        Enemy enemy = Instantiate(_enemyPrefab, _spawnPositions[spawnPoint].position, Quaternion.identity);
-        enemy.SetDirectionMove(GetDirection());
+            Enemy enemy = Instantiate(_enemyPrefab, _spawnPositions[spawnPoint].position, Quaternion.identity);
+            enemy.SetDirectionMove(GetDirection());
 
-        yield return new WaitForSeconds(_timeSpawn);
-
-        StartCoroutine(CreateEnemy());
+            yield return _timeout;
+        }
     }
 
     private Vector3 GetDirection()
